ce63872bd49feb00509b0f8f0e901e2c89ce9f29
sandrbuz <sandrbuz@mail.ru>
2021-05-02 00:09:37 +0300

    done css, html and little bit of js

---
+++ script.js
@@ -0,0 +1,10 @@
+function insert(num) {
+    document.form.textview.value = document.form.textview.value + num;
+}
+function clean() {
+    document.form.textview.value = "";
+}
+function back() {
+    var exp = document.form.textview.value;
+    document.form.textview.value = exp.substring(0, exp.length - 1);
+}
